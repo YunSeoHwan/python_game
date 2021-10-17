2b2611d5842412d44874c468b8492858474e7ba1
YunSeoHwan <48356954+YunSeoHwan@users.noreply.github.com>
2021-10-17 14:10:55 +0000

    Add weapone

--- main.py
+++ main.py
@@ -36,6 +36,23 @@ character_height = character_size[1]
 character_x = (screen_width / 2) - (character_width / 2)
 character_y = screen_height - character_height - stage_height
 
+# 무기 생성
+weapone = pygame.image.load(os.path.join(image_path, "weapon.png"))
+weapone_size = weapone.get_rect().size
+weapone_width = weapone_size[0]
+
+# 무기 발사 리스트
+weapones = []
+
+# 이동 좌표
+to_x = 0
+
+# 캐릭터 속도
+character_speed = 5
+
+# 무기 속도
+weapone_speed = 10
+
 running = True  
 while running:
   dt = clock.tick(60) 
@@ -45,14 +62,43 @@ while running:
     if event.type == pygame.QUIT:   
       running = False  
 
+    if event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_LEFT:
+        to_x -= character_speed
+      elif event.key == pygame.K_RIGHT:
+        to_x += character_speed
+      elif event.key == pygame.K_SPACE: # 무기 발사
+        weapone_x = character_x + (character_width / 2) - (weapone_width / 2)
+        weapone_y = character_y
+        weapones.append([weapone_x, weapone_y])
+
+    if event.type == pygame.KEYUP:
+      if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+        to_x = 0
+
   # 3. 게임 캐릭터 위치 정의
+  character_x += to_x
+
+  if character_x < 0:
+    character_x = 0
+  elif character_x > screen_width - character_width:
+    character_x = screen_width - character_width
+    
+  # 무기 위치 조정
+  weapones = [ [w[0], w[1] - weapone_speed] for w in weapones if w[1] > 0] # 천장에 닿으면 없애기
 
   # 4. 충돌 처리
 
   # 5. 화면 그리기
   screen.blit(background, (0, 0))
+
+  for weapone_x, weapone_y in weapones:   # 리스트이기 때문에 반복문 사용
+    screen.blit(weapone, (weapone_x, weapone_y))
+
   screen.blit(stage, (0, screen_height - stage_height))
   screen.blit(character, (character_x, character_y))
+
+  
 
   pygame.display.update()
 
